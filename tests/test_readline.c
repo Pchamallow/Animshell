8f4f27c376a9939ac6a39d98acdaf655788ba480
Steph Kloutz <stkloutz@student.42angouleme.fr>
2026-04-01 16:11:04 +0200

    01avr - 16h10 - Steph - tests:poursuite tests lexer + test ioctl

--- tests/test_readline.c
+++ tests/test_readline.c
@@ -6,7 +6,7 @@
 /*   By: stkloutz <stkloutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 22:21:25 by stkloutz          #+#    #+#             */
-/*   Updated: 2026/03/30 22:33:45 by stkloutz         ###   ########.fr       */
+/*   Updated: 2026/04/01 15:49:21 by stkloutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,60 @@
 /* --------------- test 1 ---------------------- */
 /* ********************************************* */
 
-/*#include <stdlib.h>*/
-/*#include <unistd.h>*/
-/*#include <stdio.h>*/
-/*#include <string.h>*/
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
-/*#include <readline/readline.h>*/
-/*#include <readline/history.h>*/
+#include <readline/readline.h>
+#include <readline/history.h>
 
-/*int*/
-/*main (void)*/
-/*{*/
-	/*char *input;*/
+int
+main (void)
+{
+    char *input;
 
-	/*for (;;) {*/
-		/*input = readline((char *)NULL);*/
-		/*if (input == 0)*/
-			/*break;*/
-		/*printf ("%s\n", input);*/
-		/*if (strcmp (input, "exit") == 0)*/
-			/*break;*/
-		/*free (input);*/
-	/*}*/
-	/*exit (0);*/
-/*}*/
+    for (;;) {
+        input = readline((char *)NULL);
+        if (input == 0)
+            break;
+        printf ("%s\n", input);
+        if (strcmp (input, "exit") == 0)
+            break;
+        free (input);
+    }
+    exit (0);
+}
 
 /* ********************************************* */
 /* --------------- test 2 ---------------------- */
 /* ********************************************* */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>   // For readline()
-#include <readline/history.h>    // For add_history()
+/*#include <stdio.h>*/
+/*#include <stdlib.h>*/
+/*#include <readline/readline.h>   // For readline()*/
+/*#include <readline/history.h>    // For add_history()*/
 
-int main() {
-    char *input;
+/*int main() {*/
+    /*char *input;*/
 
-    // Prompt the user and read input with readline()
-    input = readline("Enter your name: ");
+    /*// Prompt the user and read input with readline()*/
+    /*input = readline("Enter your name: ");*/
 
-    // Check if input is NULL (user pressed Ctrl+D to exit)
-    if (input == NULL) {
-        printf("\nExiting...\n");
-        return 0;
-    }
+    /*// Check if input is NULL (user pressed Ctrl+D to exit)*/
+    /*if (input == NULL) {*/
+        /*printf("\nExiting...\n");*/
+        /*return 0;*/
+    /*}*/
 
-    // Add input to history (so user can recall it with Up arrow)
-    add_history(input);
+    /*// Add input to history (so user can recall it with Up arrow)*/
+    /*add_history(input);*/
 
-    // Print the input
-    printf("Hello, %s!\n", input);
+    /*// Print the input*/
+    /*printf("Hello, %s!\n", input);*/
 
-    // Free the memory allocated by readline()
-    free(input);
+    /*// Free the memory allocated by readline()*/
+    /*free(input);*/
 
-    return 0;
-}
+    /*return 0;*/
+/*}*/
