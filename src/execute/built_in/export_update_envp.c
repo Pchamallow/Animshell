d8315cf3004020e63365dbccb40e5c78d18fa7e6
Steph Kloutz <stkloutz@student.42angouleme.fr>
2026-05-24 18:19:33 +0200

    24mai - 18h20 - Steph - fix: protection malloc in export

--- src/execute/built_in/export_update_envp.c
+++ src/execute/built_in/export_update_envp.c
@@ -6,7 +6,7 @@
 /*   By: stkloutz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 17:11:36 by stkloutz          #+#    #+#             */
-/*   Updated: 2026/05/09 17:13:30 by stkloutz         ###   ########.fr       */
+/*   Updated: 2026/05/24 18:08:47 by stkloutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,17 +39,19 @@ void	concat_var(char **envp, char *var, t_minishell *minishell)
 void	replace_var(char **envp, char *var, t_minishell *minishell)
 {
 	int		i;
+	char	*tmp;
 
 	i = found_var(envp, var);
 	if (i < 0 || !ft_env_cmp(envp[i], var) || !ft_strchr(var, '='))
 		return ;
-	free(envp[i]);
-	envp[i] = ft_strdup(var);
-	if (!envp[i])
+	tmp = ft_strdup(var);
+	if (!tmp)
 	{
 		free(envp);
 		print_error_free(minishell, "malloc error in export\n", EXIT_FAILURE);
 	}
+	free(envp[i]);
+	envp[i] = tmp;
 }
 
 void	update_var(t_minishell *minishell, char **new_envp, char *arg)
@@ -62,15 +64,18 @@ void	update_var(t_minishell *minishell, char **new_envp, char *arg)
 
 void	create_var(t_minishell *minishell, char **new_envp, char *arg, int i)
 {
+	char	*tmp;
+
 	if (concat_found(arg) >= 0)
-		new_envp[i] = new_concat(arg);
+		tmp = new_concat(arg);
 	else
-		new_envp[i] = ft_strdup(arg);
-	if (!new_envp[i])
+		tmp = ft_strdup(arg);
+	if (!tmp)
 	{
-		free_strv(new_envp);
+		free(new_envp);
 		print_error_free(minishell, "malloc error in export\n", EXIT_FAILURE);
 	}
+	new_envp[i] = tmp;
 }
 
 char	**update_envp(t_minishell *minishell, t_token *arg, int count)
