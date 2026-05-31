9aa10c2b4998142a5cb84579bdb4c96a80478c1f
stepouik <schteph_99@yahoo.com>
2026-05-31 22:10:08 +0200

    31mai - 22h10 - Steph - fix: petite erreur ctrl c en cas de multiples child

--- src/signals/signals_default.c
+++ src/signals/signals_default.c
@@ -6,7 +6,7 @@
 /*   By: pswirgie <pswirgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 21:14:00 by stkloutz          #+#    #+#             */
-/*   Updated: 2026/05/31 21:07:20 by stkloutz         ###   ########.fr       */
+/*   Updated: 2026/05/31 22:01:35 by stkloutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,9 @@ void	get_exit_status(t_minishell *minishell, pid_t last_pid)
 {
 	int		child_exit_status;
 	pid_t	wpid;
+	bool	already_signaled;
 
+	already_signaled = false;
 	wpid = waitpid(-1, &child_exit_status, 0);
 	while (wpid > 0)
 	{
@@ -65,8 +67,11 @@ void	get_exit_status(t_minishell *minishell, pid_t last_pid)
 			if (WIFEXITED(child_exit_status))
 				minishell->exec.error = WEXITSTATUS(child_exit_status);
 		}
-		if (WIFSIGNALED(child_exit_status))
+		if (WIFSIGNALED(child_exit_status) && !already_signaled)
+		{
 			get_signal_status(minishell, child_exit_status);
+			already_signaled = true;
+		}
 		wpid = waitpid(-1, &child_exit_status, 0);
 	}
 }
